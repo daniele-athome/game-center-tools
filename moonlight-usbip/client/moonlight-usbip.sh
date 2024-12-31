913f697eb94d87904d857640ac636ca1e0181cec
Daniele Ricci <daniele@casaricci.it>
2024-12-31 11:04:39 +0100

    Enumerate over USB/IP configured devices

--- moonlight-usbip/client/moonlight-usbip.sh
+++ moonlight-usbip/client/moonlight-usbip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MOONLIGHT_BIN="$(which moonlight)"
+USBIP_DEVICES_PATH="/etc/usbip/bind-devices"
 
 start_and_check() {
   if systemctl is-active --quiet "$1"; then
@@ -16,17 +17,21 @@ notify() {
   notify-send -i "${3:-moonlight}" -u "${4:-normal}" "${2:-Moonlight USB/IP}" "$1"
 }
 
-# TODO enumerate devices in the configuration folders and try to bind all of them
 
-start_and_check usbipd.service || notify "Failed to start usbipd. Controllers will not work."
-# TODO if usbipd failed, there is no need to bind devices
-start_and_check usbip-bind@ds4.service || notify "Failed to bind DualShock 4 controller."
-start_and_check usbip-bind@ds5.service || notify "Failed to bind DualSense controller."
+if ! start_and_check usbipd.service; then
+  notify "Failed to start usbipd. Controllers will not work."
+else
+  find "$USBIP_DEVICES_PATH" -type f -name '*.conf' | while read -r line; do
+    bind_name="$(basename "$line" .conf)"
+    start_and_check "usbip-bind@$bind_name.service" || notify "Failed to bind controller $bind_name."
+  done
+fi
 
 "$MOONLIGHT_BIN" "$@"
 
-# TODO enumerate devices in the configuration folders and try to unbind all of them
-systemctl stop \
-  usbipd.service \
-  usbip-bind@ds4.service \
-  usbip-bind@ds5.service
+systemctl stop usbipd.service
+
+find "$USBIP_DEVICES_PATH" -type f -name '*.conf' | while read -r line; do
+  bind_name="$(basename "$line" .conf)"
+  systemctl stop "usbip-bind@$bind_name.service"
+done
